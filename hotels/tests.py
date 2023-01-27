e170eb94285b3b76cdbcb282d2ea6d2f069c9c59
Alejandro Alomia <alejandroalomia730@gmail.com>
2023-01-27 02:50:24 -0500

    add data to the Hotel model and tests

--- hotels/tests.py
+++ hotels/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Hotel
+
+
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.hotel = Hotel.objects.create(
+            name="Hotel Casa Pestagua",
+            code="HTCPE-08"
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.hotel.name, "Hotel Casa Pestagua")
+        self.assertEqual(self.hotel.code, "HTCPE-08")
+        self.assertEqual(str(self.hotel), "Hotel Casa Pestagua")
